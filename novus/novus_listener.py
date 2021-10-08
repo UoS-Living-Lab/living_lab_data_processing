56b1febd156ccb8b015f903c60708514caaa9427
Ethan Bellmer <e.bellmer@outlook.com>
2021-10-08 11:04:45 +0100

    Initial commit

---
+++ novus/novus_listener.py
@@ -0,0 +1,16 @@
+"""
+Listens for POST requests from the intermediary device
+"""
+
+
+__author__ = "Ethan Bellmer"
+__version__ = "0.1"
+
+
+import flask
+import json
+
+
+# Main body
+if __name__ == '__main__':
+	print('NOVUS Listener')
---
+++ novus/novus_sender.py
@@ -0,0 +1,18 @@
+"""
+Reads CSV files stored on the file system, 
+formats them into a JSON structure, 
+and POSTs them to the off-site listener. 
+"""
+
+
+__author__ = "Ethan Bellmer"
+__version__ = "0.1"
+
+import flask
+import json
+
+
+# Main body
+if __name__ == '__main__':
+	print('NOVUS Sender')
+
